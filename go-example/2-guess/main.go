d4c9abd63cc8bca9bac8c9e7fc184b47c2b3dafe
Marchons <d1286360646@outlook.com>
2023-05-12 23:33:14 +0800

    feat: change guess game into fmt.scanf mode

--- go-example/2-guess/main.go
+++ go-example/2-guess/main.go
@@ -11,6 +11,46 @@ import (
 )
 
 func main() {
+	GameV2()
+}
+
+func GameV2() {
+	maxNum := 100
+	rand.Seed(time.Now().UnixNano())
+	secretNum := rand.Intn(maxNum)
+	fmt.Println("The secret number is ", secretNum)
+	fmt.Println("Please input your guess: ")
+	for {
+
+		var guess int
+		fmt.Scanf("%d\n", &guess)
+		// input, err := reader.ReadString('\n')
+		// if err != nil {
+		// 	fmt.Println("An error occured while reading input, please try again", err)
+		// 	continue
+		// }
+		// input = strings.TrimSuffix(input, "\r\n")
+		// a,_:=strconv.Atoi(input)
+		// fmt.Println(input);
+		// guess, err := strconv.Atoi(input)
+		// // fmt.Println(guess);
+		// if err != nil {
+		// 	fmt.Println("invalid input , please input an integer")
+		// 	continue
+		// }
+		fmt.Println("your guess is ", guess)
+		if guess > secretNum {
+			fmt.Println("your guess is bigger than the secretNum. Please try again")
+		} else if guess < secretNum {
+			fmt.Println("your guess is smaller than the secretNum. Please try again")
+		} else {
+			fmt.Println("Congratulation! you are right!")
+			break
+		}
+	}
+}
+
+func Game() {
 	maxNum := 100
 	rand.Seed(time.Now().UnixNano())
 	secretNum := rand.Intn(maxNum)
